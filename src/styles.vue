2b577546dbb63c56fda62b227c256e499092aa0d
Takahiro Yaota <z@kuro.red>
2019-06-08 13:38:06 +0900

    Set color to custom properties

--- src/styles.vue
+++ src/styles.vue
@@ -1,1 +1,17 @@
 <style src="@vue/ui/dist/vue-ui.css"></style>
+<style>
+:root {
+  /* https://github.com/vuejs/ui/blob/822e0ba21ef4cc856b981da1a46f25ef002eda97/src/style/colors.styl */
+  --vue-ui-color-light          : #ffffff;
+  --vue-ui-color-light-neutral  : #e4f5ef;
+  --vue-ui-color-dark           : #2c3e50;
+  --vue-ui-color-dark-neutral   : #4f6f7f;
+  --vue-ui-color-darker         : #1d2935;
+  --vue-ui-color-primary        : #42b983;
+  --vue-ui-color-accent         : #6806c1;
+  --vue-ui-color-danger         : #e83030;
+  --vue-ui-color-warning        : #ea6e00;
+  --vue-ui-color-info           : #03c2e6;
+  --vue-ui-color-success        : #42b983;
+}
+</style>
