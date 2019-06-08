d19badf108aa308b11f4559e0ce4dc6ea833ea2d
Takahiro Yaota <z@kuro.red>
2019-06-08 13:38:41 +0900

    Add VueCard

---
+++ src/components/VueCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="vue-ui-card card">
+    <h2 v-if="title" class="vue-ui-card-title">{{ title }}</h2>
+    <div class="vue-ui-card-content">
+      <slot />
+    </div>
+    <div class="vue-ui-card-actions">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueCard',
+  props: {
+    title: String,
+  }
+}
+</script>
+
+<style scoped>
+.vue-ui-title {}
+.vue-ui-card {
+  box-sizing: border-box;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  background-color: var(--vue-ui-color-light-neutral);
+}
+
+.vue-ui-card-actions {
+  display: flex;
+  flex-direction: column;
+}
+</style>
